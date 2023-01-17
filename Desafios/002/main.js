2dbb74ad906e65cce3d9dadc58b70a84438887f9
MarcusLara <101606567+marcuslaraa@users.noreply.github.com>
2023-01-17 19:48:45 -0300

    Update main.js

--- Desafios/002/main.js
+++ Desafios/002/main.js
@@ -1,4 +1,5 @@
 const notas = document.querySelector('#calcular');
+const acessEmail = document.querySelector('#email-acess');
 
 notas.addEventListener('click', () => {
   console.log('fui clicado');
